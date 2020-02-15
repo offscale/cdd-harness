cce4f0a822f948f6b0c0fdb3e91d2b6808a2ea42
Rob Saunders <rob@wearebrandnew.com>
2020-02-15 14:41:11 +1100

    basic socket call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+pub fn main() {
+    // ws::listen("127.0.0.1:7779", |out| move |msg| out.send(msg));
+
+    if let Err(error) = ws::connect("ws://127.0.0.1:7779", |out| {
+        if out.send("Hello WebSocket").is_err() {
+            println!("Websocket couldn't queue an initial message.");
+        } else {
+            println!("Client sent message 'Hello WebSocket'. ");
+        }
+
+        move |msg| {
+            println!("msg: {:?}", msg);
+            out.close(ws::CloseCode::Normal)
+        }
+    }) {
+        // Inform the user of failure
+        println!("Failed to create WebSocket due to: {:?}", error);
+    };
 }
+
+// fn rpc_call(method: &str, )
