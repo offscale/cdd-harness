38175f51b314c73c9ca39cd7bc5b7f66dd075c2e
Rob Saunders <rob@wearebrandnew.com>
2020-02-22 16:17:54 +1100

    command line argument for server

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ mod rpc;
 mod tests;
 
 pub fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let hostname: &str = match args.get(1) {
+        Some(arg) => arg,
+        None => "ws://127.0.0.1:7779",
+    };
+
     let project = cdd::Project {
         info: None,
         models: vec![cdd::Model {
@@ -19,7 +26,9 @@ pub fn main() {
         }],
     };
 
-    let result = methods::update("ws://127.0.0.1:7779", "", project);
+    println!("trying {}", hostname);
+
+    let result = methods::update(hostname, "", project);
 
     println!("--> {:?}", result);
     println!("{:#}", result.unwrap().code);
