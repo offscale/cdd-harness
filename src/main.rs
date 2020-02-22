a8ad337343f279f4a96267b6cf17f10ff9faf20a
Rob Saunders <rob@wearebrandnew.com>
2020-02-22 14:36:11 +1100

    main cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,7 @@ mod methods;
 mod rpc;
 mod tests;
 
-const CODE: &str = r#"
-use diesel::Queryable;
-
-#[derive(Queryable, Debug)]
-pub struct Pet {
-	pub id: i32,
-	pub name: String,
-	pub tag: String,
-}
-
-#[derive(Queryable, Debug)]
-pub struct Error {
-	pub code: i32,
-	pub message: String,
-}
-"#;
-
 pub fn main() {
-    // println!("--> {:?}", rpc::parse("ws://127.0.0.1:7779", CODE));
-
     let project = cdd::Project {
         info: None,
         models: vec![cdd::Model {
